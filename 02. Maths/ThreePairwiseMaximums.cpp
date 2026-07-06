0b74192e1ebd6db93406cbeec935589d1761a68a
MRIDUL896 <156008897+MRIDUL896@users.noreply.github.com>
2026-07-06 23:53:35 +0530

    06/07/26

---
+++ 02. Maths/ThreePairwiseMaximums.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        vector<int> arr(3);
+        for(int i=0;i<3;i++) cin>>arr[i];
+        sort(arr.begin(),arr.end());
+        if(arr[1] != arr[2]) cout<<"NO";
+        else{
+            cout<<"YES"<<endl;
+            cout<<arr[0]<<" "<<arr[0]<<" "<<arr[2];
+        }
+        cout<<endl;
+    }
+}
